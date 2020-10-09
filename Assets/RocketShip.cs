576b61cc109ad77f950ffc39e0c6029ab3912b16
dotdotdotdavin <davinsolano@gmail.com>
2020-10-09 14:00:20 +0800

    Physics and input

---
+++ Assets/RocketShip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up) ;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
