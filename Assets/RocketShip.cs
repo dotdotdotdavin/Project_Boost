786fbf7266105326ea001bb5027ffeb313ce7965
dotdotdotdavin <davinsolano@gmail.com>
2020-10-10 12:03:28 +0800

    audio and thrust

--- Assets/RocketShip.cs
+++ Assets/RocketShip.cs
@@ -7,30 +7,51 @@ public class RocketShip : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float rocketThrust = 100f;
+    [SerializeField] float mainThrust = 8f;
+    float rotationSpeed;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up) ;
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if(!audioSource.isPlaying){
+              audioSource.Play();
+            }
         }
+        else{
+          audioSource.Stop();
+        }
+    }
 
+    private void Rotate()
+    {
+      rigidBody.freezeRotation = true;
+
+        rotationSpeed = rocketThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
+
+      rigidBody.freezeRotation = false;
     }
 }
